1b1742ce698e5d69dc826e580022d8aeef63b17b
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-03-07 21:20:21 +0300

    games pages, move stuff over to components, added featured games to the emulation page of NES

--- src/lib/data/consoles.ts
+++ src/lib/data/consoles.ts
@@ -4,9 +4,40 @@ import Controller3 from "$assets/controller/3.svg"
 import Controller4 from "$assets/controller/4.svg"
 import type { SvgComponent } from "astro/types"
 
-export const supportedConsoles: { id:string, name: string, playUrl: string, description: string, imgUrl: SvgComponent, color: string, cssClass: string }[] = [
-    { id:"nes", name: "NES", playUrl: "/play/nes", description: "a.", imgUrl: Controller1, color: "oklch(58.6% 0.253 17.585)", cssClass: "col-span-2" },
-    { id:"snes", name: "SNES", playUrl: "/play/snes", description: "a.", imgUrl: Controller2, color: "oklch(62.7% 0.194 149.214)", cssClass: "col-span-2" },
-    { id:"gameboy", name: "Gameboy", playUrl: "/play/gameboy", description: "gameboy.", imgUrl: Controller3, color: "#94c138", cssClass: "col-span-1 row-span-2" },
-    { id:"ps", name: "Playstation", playUrl: "/play/playstation", description: "playstation.", imgUrl: Controller4, color: "oklch(54.6% 0.245 262.881)", cssClass: "col-span-3 row-span-1 row-start-3 col-start-2" }
+
+export const supportedConsoles: { id: string, name: string, playUrl: string, description: string, imgUrl: SvgComponent, color: string, cssClass: string }[] = [
+    { id: "nes", name: "NES", playUrl: "/play/nes", description: "a.", imgUrl: Controller1, color: "oklch(58.6% 0.253 17.585)", cssClass: "col-span-2" },
+    { id: "snes", name: "SNES", playUrl: "/play/snes", description: "a.", imgUrl: Controller2, color: "oklch(62.7% 0.194 149.214)", cssClass: "col-span-2" },
+    { id: "gameboy", name: "Gameboy", playUrl: "/play/gameboy", description: "gameboy.", imgUrl: Controller3, color: "#94c138", cssClass: "col-span-1 row-span-2" },
+    { id: "ps", name: "Playstation", playUrl: "/play/playstation", description: "playstation.", imgUrl: Controller4, color: "oklch(54.6% 0.245 262.881)", cssClass: "col-span-3 row-span-1 row-start-3 col-start-2" },
+    { id: "sega-saturn", name:"Sega Saturn", playUrl: "/play/sega-saturn", description: "description for sega saturn.", imgUrl: Controller4, color: "oklch(54.6% 0.245 262.881)", cssClass: "col-span-3 row-span-1 row-start-3 col-start-2" }
 ]
+
+import { type CollectionEntry, getCollection } from "astro:content";
+type RomGame = CollectionEntry<"romgames">;
+
+export async function getGamesOfAllConsolesAsEntries() {
+    const allRomGames = await getCollection("romgames");
+
+    type RomGame = CollectionEntry<"romgames">;
+    type GenericDict<K extends string, V> = Record<K, V>;
+    let romGamesByConsoles: GenericDict<string, Array<RomGame>> = {};
+
+    for (const rg of allRomGames) {
+        if (romGamesByConsoles[rg.data.category] == undefined) {
+            romGamesByConsoles[rg.data.category] = [];
+        }
+        romGamesByConsoles[rg.data.category].push(rg);
+    }
+
+    const romGamesToMap = Object.entries(romGamesByConsoles);
+
+    return romGamesToMap;
+}
+
+export async function getGamesOfConsoleAsEntries(consoleCategory: string) {
+    const AllRomGamesEntries = getGamesOfAllConsolesAsEntries();
+
+    const games = (await AllRomGamesEntries).find((e) => e[0] == consoleCategory)
+    return games;
+}
--- src/lib/data/navigation.ts
+++ src/lib/data/navigation.ts
@@ -1,6 +1,6 @@
 export const headerNavigationLinks: { url: string, text: string }[] = [
     { url: "/", text: "Home" },
-    { url: "/#faq", text: "Frequently Asked Questions" },
     { url: "/play", text: "Play" },
-    { url: "/games", text: "Games" }
+    { url: "/games", text: "Games" },
+    { url: "/#faq", text: "Frequently Asked Questions" },
 ]
