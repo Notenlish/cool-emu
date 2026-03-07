309c7ea3913904add669292dbc4398c0c7cddbeb
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-03-07 14:26:19 +0300

    new /play page, change some sizing issues regarding games

--- src/lib/data/navigation.ts
+++ src/lib/data/navigation.ts
@@ -1,5 +1,6 @@
 export const headerNavigationLinks: { url: string, text: string }[] = [
     { url: "/", text: "Home" },
     { url: "/#faq", text: "Frequently Asked Questions" },
+    { url: "/play", text: "Play" },
     { url: "/games", text: "Games" }
 ]
